7141d4a8fc29bc1a8c7be5f87cf783cf29070aba
Wayne Thackston <wayne.thackston@loop.colum.edu>
2017-11-15 22:23:44 -0600

    ere

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -7,14 +7,20 @@ public class ArrowScript : MonoBehaviour {
    
     Rigidbody2D rb;
     public Vector2 dir = new Vector2(0, 0);
-    
+
+    private AudioSource audioSource;
+
     private NinjaControllerScript player;
+    private BoxCollider2D box;
+    private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        
+        audioSource = GetComponent<AudioSource>();
         player = GameObject.FindObjectOfType<NinjaControllerScript>();
+        box = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
@@ -28,14 +34,15 @@ public class ArrowScript : MonoBehaviour {
         
         if (other.gameObject.tag == "Player")
         {
-
+            audioSource.Play();
             player.myRigidBody2D.constraints = RigidbodyConstraints2D.FreezePositionX;
 
             player.anim.SetBool("Dead", true);
 
             player.dead = true;
-
-            Destroy(gameObject);
+            box.enabled = false;
+            spriteRenderer.enabled = false;
+            
         }
         else
         {
